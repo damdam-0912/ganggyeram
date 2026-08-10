35edef7fd76bf513396fb709dee5a890b7160380
damdam-0912 <spspoi1015@gmail.com>
2026-08-10 10:16:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,5 @@
-const CACHE='ganggyeram-v3-warm';
-const ASSETS=[
-  './',
-  './index.html',
-  './styles.css?v=3',
-  './app.js',
-  './manifest.webmanifest?v=3',
-  './icons/icon-192.png?v=3',
-  './icons/icon-512.png?v=3',
-  './icons/icon-180.png?v=3'
-];
-self.addEventListener('install',e=>e.waitUntil(
-  caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
-));
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-    .then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-      const clone=res.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,clone));
-      return res;
-    }).catch(()=>caches.match('./index.html')))
-  );
-});
+const CACHE='ganggyeram-v4-nai-meta';
+const ASSETS=['./','./index.html','./styles.css?v=4','./app.js?v=4','./manifest.webmanifest?v=4','./icons/icon-192.png?v=4','./icons/icon-512.png?v=4','./icons/icon-180.png?v=4'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return res}).catch(()=>caches.match('./index.html'))))});
